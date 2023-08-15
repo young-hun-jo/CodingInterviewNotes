77174738e599aaed6bc90e1be65ba23626001949
Younghun <54783194+young-hun-jo@users.noreply.github.com>
2023-08-15 21:32:06 +0900

    [unrated] Title: 추억 점수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.py
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.py
@@ -1,15 +1,3 @@
 def solution(name, yearning, photo):
-    dic = dict(zip(name, yearning))
     answer = []
-    # max : 100 x 100 = 10000
-    for p in photo: # p: List[str]
-        score = 0
-        for person in p:
-            if person in dic.keys():
-                score += dic[person]
-            else:
-                continue
-        answer.append(score)
     return answer
-            
-        
